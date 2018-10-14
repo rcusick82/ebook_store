555b84cec650c0c06fd0c8a03d7a53a1b7a449f3
Ryan Cusick <rcusick82@gmail.com>
2018-10-14 15:14:59 -0400

    added models -User/Author/Sale

---
+++ db/migrate/20181014185529_create_sales.rb
@@ -0,0 +1,11 @@
+class CreateSales < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sales do |t|
+        t.integer :price
+        t.integer :quantity
+      
+
+      t.timestamps
+    end
+  end
+end
