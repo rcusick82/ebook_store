96c1f33469ce972db2170ade7cef6b7b441261fd
Ryan Cusick <rcusick82@gmail.com>
2018-10-15 10:00:22 -0400

    added null false to book model

--- db/migrate/20181014185529_create_sales.rb
+++ db/migrate/20181014185529_create_sales.rb
@@ -1,9 +1,8 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
-        t.integer :price
-        t.integer :quantity
-      
+        t.integer :price, null: false
+        t.integer :quantity, null: false
 
       t.timestamps
     end
