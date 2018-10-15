2e515c9b64c3637d99ff649576c526d2c9e5a73c
Ryan Cusick <rcusick82@gmail.com>
2018-10-15 16:58:40 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "home#index"
+
+namespace :admin do
+  resources :users, :authors, :books
+end
+
+resources :authors, only: [:show]
+resources :books, only: [:index, :show]
+resources :home, only: [:show]
 end
