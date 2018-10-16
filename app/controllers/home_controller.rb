fe651a01c91c8d71d57606ada8252e7732d869e7
Ryan Cusick <rcusick82@gmail.com>
2018-10-16 15:33:45 -0400

    fixed css file structure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    redirect_to_root_path
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :name, :avatar)
+  end
 end
---
+++ app/controllers/users.rb
@@ -0,0 +1,3 @@
+class UsersController < ApplicationController
+has_one_attached :avatar 
+end
